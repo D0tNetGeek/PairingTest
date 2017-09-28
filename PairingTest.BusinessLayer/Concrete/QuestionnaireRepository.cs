13198333110f2c2b4d871ac11ed98d9b3d138a05
D0tNetGeek <vmalvi2003@yahoo.com>
2017-09-29 00:31:16 +0100

    Add files via upload

---
+++ PairingTest.BusinessLayer/Concrete/QuestionnaireRepository.cs
@@ -0,0 +1,36 @@
+﻿using PairingTest.BusinessLayer.Service;
+using PairingTest.DAL.Services;
+using PairingTest.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PairingTest.BusinessLayer.Concrete
+{
+    public class QuestionnaireRepository : IQuestionnaireRepository
+    {
+        private readonly IQuestionnaireDal _questionnaireDal;
+
+        public QuestionnaireRepository(IQuestionnaireDal questionnaireDal)
+        {
+            _questionnaireDal = questionnaireDal;
+        }
+
+        public async Task<QuestionnaireViewModel> GetQuestionaire()
+        {
+            return await _questionnaireDal.GetQuestionnaire();
+        }
+
+        public async Task<QuestionAnswerViewModel> GetQuestionAnswerModel()
+        {
+            var questions = await GetQuestionaire();
+
+            return new QuestionAnswerViewModel
+            {
+                QuestionnaireTitle = questions.QuestionnaireTitle,
+                QuestionsAnswers =
+                    questions.QuestionsText.Select(x => new QuestionAnswer {Question = x, Answer = ""}).ToList()
+            };
+        }
+    }
+}
---
+++ PairingTest.BusinessLayer/Service/IQuestionnaireRepository.cs
@@ -0,0 +1,11 @@
+﻿using PairingTest.Models;
+using System.Threading.Tasks;
+
+namespace PairingTest.BusinessLayer.Service
+{
+    public interface IQuestionnaireRepository
+    {
+        Task<QuestionnaireViewModel> GetQuestionaire();
+        Task<QuestionAnswerViewModel> GetQuestionAnswerModel();
+    }
+}
